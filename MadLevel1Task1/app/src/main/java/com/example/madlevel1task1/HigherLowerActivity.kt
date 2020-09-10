b0d03bf72e0ede54ef614697deb0fc39f9ee9040
Ruben <ruben.oosterwijk@hva.nl>
2020-09-10 17:40:23 +0200

    Added Madlevel1Task

---
+++ MadLevel1Task1/app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madlevel1task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HigherLowerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_higher_lower)
+    }
+}
